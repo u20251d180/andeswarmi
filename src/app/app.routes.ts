ac92bd85816ab289d8c2d98b3820e2c55af33fec
Diego <diego@example.com>
2025-09-23 00:15:32 -0500

    added recovery and register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Recovery } from './pages/recovery/recovery';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    
     {path : 'login', component: Login},
-    {path : 'home', component: Home}
+    {path : 'home', component: Home},
+    {path : 'recover-password', component: Recovery},
+    {path : 'register', component: Register},
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
---
+++ src/app/pages/recovery/recovery.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recovery',
+  imports: [],
+  templateUrl: './recovery.html',
+  styleUrl: './recovery.css'
+})
+export class Recovery {
+
+}
