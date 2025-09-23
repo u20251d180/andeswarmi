caaff4b96807ec530e133dfb5adf24ff7118e8f0
Diego <diego@example.com>
2025-09-22 22:18:31 -0500

    primer checkin, creacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Landing } from './pages/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'login', component: Login},
+    {path : 'home', component: Landing}
+];
