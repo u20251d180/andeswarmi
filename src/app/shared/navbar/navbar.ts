af31339bf7755a11d50bd54e761012ef771a0444
Alvarows20 <alvarojpt04.ws@gmail.com>
2025-09-27 13:48:02 -0500

    feat: responsive navbar with sidebar + layout and style updates

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -8,4 +8,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class Navbar {}
+export class Navbar {
+  sidebarOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+}
